181038c458e73571c7143d762e560aa199eab021
Allan Muturi <amuturi01@gmail.com>
2023-02-13 10:09:21 +0300

    add signup widget

--- lib/screens/login/component/login_widget.dart
+++ lib/screens/login/component/login_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:furniture/auth/authenticate.dart';
 import 'package:furniture/components/horizontaltext.dart';
 import 'package:furniture/components/texttitle.dart';
 import 'package:furniture/screens/signup/signup.dart';
@@ -24,111 +25,122 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextTitle(
-              text: "Login",
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            TextFormField(
-              controller: usernameController,
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Please provide a username";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  labelText: "Username",
-                  border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.defaultSize * 8),
-                      borderSide: BorderSide.none)),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            TextFormField(
-              controller: passwordController,
-              obscureText: obscurePass,
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Please provide a password";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  labelText: "Password",
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscurePass = !obscurePass;
-                        });
-                      },
-                      icon: obscurePass
-                          ? const Icon(Icons.visibility_off)
-                          : const Icon(Icons.visibility)),
-                  border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.defaultSize * 8),
-                      borderSide: BorderSide.none)),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize,
-            ),
-            TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Forgot Password ?",
-                  style: TextStyle(color: kPrimaryColor),
-                )),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: kPrimaryColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5)),
-                child: Text(
-                  'Continue to Login',
-                  style: TextStyle(
-                      fontSize: SizeConfig.defaultSize * 1.6,
-                      fontWeight: FontWeight.bold),
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextTitle(
+                text: "Login",
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
+              ),
+              TextFormField(
+                controller: usernameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please provide email address";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    filled: true,
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(SizeConfig.defaultSize * 8),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: obscurePass,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please provide a password";
+                    }
+                    return null;
+                  },
+                decoration: InputDecoration(
+                    filled: true,
+                    labelText: "Password",
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            obscurePass = !obscurePass;
+                          });
+                        },
+                        icon: obscurePass
+                            ? const Icon(Icons.visibility_off)
+                            : const Icon(Icons.visibility)),
+                    border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(SizeConfig.defaultSize * 8),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Forgot Password ?",
+                    style: TextStyle(color: kPrimaryColor),
+                  )),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Authenticate().loginUser(
+                          usernameController.text, passwordController.text, context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5)),
+                  child: Text(
+                    'Continue to Login',
+                    style: TextStyle(
+                        fontSize: SizeConfig.defaultSize * 1.6,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            HorizontalTextLine(height: SizeConfig.defaultSize, label: "Or"),
-            SizedBox(
-              width: double.infinity,
-              child: SignInButton(
-                Buttons.Google,
-                onPressed: () {},
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
               ),
-            ),
-            Row(
-              children: [
-                const Text("Don\'nt have an account ?"),
-                TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return const Signup();
-                  }));
-                }, child: const Text("Signup"))
-              ],
-            )
-          ],
+              HorizontalTextLine(height: SizeConfig.defaultSize, label: "Or"),
+              SizedBox(
+                width: double.infinity,
+                child: SignInButton(
+                  Buttons.Google,
+                  onPressed: () {},
+                ),
+              ),
+              Row(
+                children: [
+                  const Text("Don\'nt have an account ?"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const Signup();
+                        }));
+                      },
+                      child: const Text("Signup"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup/component/signupwidget.dart
+++ lib/screens/signup/component/signupwidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:furniture/auth/authenticate.dart';
 import 'package:furniture/screens/login/login_screen.dart';
 
 import '../../../components/horizontaltext.dart';
@@ -25,128 +26,135 @@ class _SignUpWidgetState extends State<SignUpWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextTitle(
-              text: "Sign Up",
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2.5,
-            ),
-            TextFormField(
-              controller: usernameController,
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Please provide a username";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  labelText: "Username",
-                  border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.defaultSize * 8),
-                      borderSide: BorderSide.none)),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            TextFormField(
-              controller: emailController,
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Please provide a password";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  labelText: "Email",
-                  border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.defaultSize * 8),
-                      borderSide: BorderSide.none)),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            TextFormField(
-              controller: passwordController,
-                obscureText: obscurePass,
-                validator: (value){
-                  if(value!.isEmpty){
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextTitle(
+                text: "Sign Up",
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2.5,
+              ),
+              TextFormField(
+                controller: usernameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please provide a username";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    filled: true,
+                    labelText: "Username",
+                    border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(SizeConfig.defaultSize * 8),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
+              ),
+              TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value!.isEmpty) {
                     return "Please provide a password";
                   }
                   return null;
                 },
                 decoration: InputDecoration(
                     filled: true,
-                    labelText: "Password",
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            obscurePass = !obscurePass;
-                          });
-                        },
-                        icon: obscurePass
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility)),
+                    labelText: "Email",
                     border: OutlineInputBorder(
                         borderRadius:
                             BorderRadius.circular(SizeConfig.defaultSize * 8),
-                        borderSide: BorderSide.none))),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: kPrimaryColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                    padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5)),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                      fontSize: SizeConfig.defaultSize * 1.6,
-                      fontWeight: FontWeight.bold),
+                        borderSide: BorderSide.none)),
+              ),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
+              ),
+              TextFormField(
+                  controller: passwordController,
+                  obscureText: obscurePass,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please provide a password";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      filled: true,
+                      labelText: "Password",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              obscurePass = !obscurePass;
+                            });
+                          },
+                          icon: obscurePass
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility)),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(SizeConfig.defaultSize * 8),
+                          borderSide: BorderSide.none))),
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Authenticate().registerUser(
+                          emailController.text, passwordController.text, context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5)),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        fontSize: SizeConfig.defaultSize * 1.6,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: SizeConfig.defaultSize * 2,
-            ),
-            HorizontalTextLine(height: SizeConfig.defaultSize, label: "Or"),
-            SizedBox(
-              width: double.infinity,
-              child: SignInButton(
-                Buttons.Google,
-                onPressed: () {},
+              SizedBox(
+                height: SizeConfig.defaultSize * 2,
               ),
-            ),
-            Row(
-              children: [
-                const Text("Already have an account ?"),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()),
-                          (route) => false);
-                    },
-                    child: const Text("Login"))
-              ],
-            )
-          ],
+              HorizontalTextLine(height: SizeConfig.defaultSize, label: "Or"),
+              SizedBox(
+                width: double.infinity,
+                child: SignInButton(
+                  Buttons.Google,
+                  onPressed: () {},
+                ),
+              ),
+              Row(
+                children: [
+                  const Text("Already have an account ?"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()),
+                            (route) => false);
+                      },
+                      child: const Text("Login"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
